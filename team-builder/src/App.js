11bd92c8f75bb45360e2161e92d4c6c5224df7b9
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-08-13 19:44:18 -0700

    App functioning! MVP completed

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import Form from './components/Form.js'
+import TeamMembers from './components/TeamMembers.js'
+import data from './data.js'
+
 import './App.css';
 
 function App() {
+  const [teamMember, setTeamMember] = useState(data);
+  
+  const addNewMember = member => {
+    setTeamMember([ ...teamMember, member ])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Team Build</h1>
+      <Form addNewMember={addNewMember}/>
+      <TeamMembers teamList={teamMember}/>
     </div>
-  );
+  )
 }
 
 export default App;
